4327c74f3707773bd7dbfe99bb6a2f1d1182af9f
Ninisbeth Segovia Noguera <ninisbeth_segovia@hotmail.com>
2019-08-13 13:51:03 -0500

    CRUD con EF con .NET Framework completado

--- MascotasApp/MascotasApp/Controllers/MascotaController.cs
+++ MascotasApp/MascotasApp/Controllers/MascotaController.cs
@@ -9,20 +9,29 @@ namespace MascotasApp.Controllers
 {
     public class MascotaController : Controller
     {
-        const string nombreSesion = "ListadoMascota";
+        //const string nombreSesion = "ListadoMascota"; PARA TRABAJAR CON VARIABLES DE SESSION
+        private MascotasConnection BD;
+
+        public MascotaController()
+        {
+            BD = new MascotasConnection();
+        }
         public ActionResult Index()
         {
-            if (Session[nombreSesion] == null)
-            {
-                var listadoMascotas = ObtenerMascotas();
-                Session[nombreSesion] = listadoMascotas;
-                return View(listadoMascotas);
-            }
-            else
-            {
-                var listadoMascotas = (List<Mascota>)Session[nombreSesion];
-                return View(listadoMascotas.OrderBy(x=> x.Id).ToList());
-            }
+            var listado = BD.Mascota.ToList();
+            return View(listado);
+
+            //if (Session[nombreSesion] == null)
+            //{
+            //    var listadoMascotas = ObtenerMascotas();
+            //    Session[nombreSesion] = listadoMascotas;
+            //    return View(listadoMascotas);
+            //}
+            //else
+            //{
+            //    var listadoMascotas = (List<Mascota>)Session[nombreSesion];
+            //    return View(listadoMascotas.OrderBy(x=> x.Id).ToList());
+            //}
         }
 
         public ActionResult Nuevo()
@@ -32,44 +41,62 @@ namespace MascotasApp.Controllers
         [HttpPost]
         public ActionResult Nuevo(Mascota mascota)
         {
-            var listadoMascotas = (List<Mascota>)Session[nombreSesion];
-            listadoMascotas.Add(mascota);
-            Session[nombreSesion] = listadoMascotas;
+            BD.Mascota.Add(mascota);
+            BD.SaveChanges();
+            //var listadoMascotas = (List<Mascota>)Session[nombreSesion];
+            //listadoMascotas.Add(mascota);
+            //Session[nombreSesion] = listadoMascotas;
             return RedirectToAction("Index");
         }
-        private List<Mascota> ObtenerMascotas()
-        {
-            var Listado = new List<Mascota>();
-            Listado.Add(new Mascota { Id = 1, Edad = 2, Nombre = "Trusty", NombreDueno = "Nini", Raza = "Perro" });
-            Listado.Add(new Mascota { Id = 2, Edad = 8, Nombre = "Tonti", NombreDueno = "victor", Raza = "Gato" });
-            Listado.Add(new Mascota { Id = 3, Edad = 1, Nombre = "Paco", NombreDueno = "Maria", Raza = "Loro" });
+        //private List<Mascota> ObtenerMascotas()
+        //{
+        //    var Listado = new List<Mascota>();
+        //    Listado.Add(new Mascota { Id = 1, Edad = 2, Nombre = "Trusty", NombreDueno = "Nini", Raza = "Perro" });
+        //    Listado.Add(new Mascota { Id = 2, Edad = 8, Nombre = "Tonti", NombreDueno = "victor", Raza = "Gato" });
+        //    Listado.Add(new Mascota { Id = 3, Edad = 1, Nombre = "Paco", NombreDueno = "Maria", Raza = "Loro" });
 
-            return Listado;
-        }
+        //    return Listado;
+        //}
 
         public ActionResult Actualizar(int id)
         {
-            var listadoMascotas = (List<Mascota>)Session[nombreSesion];
-            var mascota = listadoMascotas.FirstOrDefault(x => x.Id == id);
+            var mascota = BD.Mascota.FirstOrDefault(x => x.id == id);
             return View(mascota);
+
+            //var listadoMascotas = (List<Mascota>)Session[nombreSesion];
+            //var mascota = listadoMascotas.FirstOrDefault(x => x.Id == id);
+            //return View(mascota);
         }
 
         [HttpPost]
         public ActionResult Actualizar(Mascota mascota)
         {
-            var listadoMascotas = (List<Mascota>)Session[nombreSesion];
-            var mascotaActualizar = listadoMascotas.FirstOrDefault(x => x.Id == mascota.Id);
-            listadoMascotas.Remove(mascotaActualizar);
-            listadoMascotas.Add(mascota);
+            var mascotaActualizar = BD.Mascota.FirstOrDefault(x => x.id == mascota.id);
+            mascotaActualizar.Edad = mascota.Edad;
+            mascotaActualizar.Nombre = mascota.Nombre;
+            mascotaActualizar.NombreDueno = mascota.NombreDueno;
+            mascotaActualizar.Raza = mascota.Raza;
+
+            BD.SaveChanges();
             return RedirectToAction("Index");
+
+            //var listadoMascotas = (List<Mascota>)Session[nombreSesion];
+            //var mascotaActualizar = listadoMascotas.FirstOrDefault(x => x.Id == mascota.Id);
+            //listadoMascotas.Remove(mascotaActualizar);
+            //listadoMascotas.Add(mascota);
+            //return RedirectToAction("Index");
         }
 
         public ActionResult Eliminar(int id)
         {
-            var listadoMascotas = (List<Mascota>)Session[nombreSesion];
-            var mascotaActualizar = listadoMascotas.FirstOrDefault(x => x.Id == id);
-            listadoMascotas.Remove(mascotaActualizar);
+            var mascotaEliminar = BD.Mascota.FirstOrDefault(x => x.id == id);
+            BD.Mascota.Remove(mascotaEliminar);
+            BD.SaveChanges();
             return RedirectToAction("Index");
+            //var listadoMascotas = (List<Mascota>)Session[nombreSesion];
+            //var mascotaActualizar = listadoMascotas.FirstOrDefault(x => x.Id == id);
+            //listadoMascotas.Remove(mascotaActualizar);
+            //return RedirectToAction("Index");
         }
     }
 }
